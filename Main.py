3343bdd5373b099a9a48d597257acc116c49d270
Mohamed Amine Taif <99884252+TaifMohamedAmine@users.noreply.github.com>
2023-06-02 04:59:44 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -11,9 +11,9 @@ This is the main execution file
 env = Environment(grid_length=50, grid_width=50, num_agents=1000, image_path="Projet pfa/images/helloo.png")
 
 # Now we instanciate our algorithm
-learning_algo = IQL(env, learning_rate=0.6, discount_rate=0.9, epsilon=1,  max_iter=2000 ,  exploration_depth = 2000)
+learning_algo = IQL(env, learning_rate=0.6, discount_rate=0.9, epsilon=1,  max_iter=100 ,  exploration_depth = 1000)
 
-learning_algo.main(1000)
+learning_algo.main(100)
 
 
 
